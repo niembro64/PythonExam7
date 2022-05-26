848f1dbc03df8529514dee63beec71794aa0fdc0
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-05-26 00:22:38 -0400

    qr drop down

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -12,4 +12,34 @@
     };
 })();
 
-console.log("TEST");
+var m = document.getElementById("mobileqr");
+var q = document.getElementById("qrqr");
+
+var clicked = true;
+function growQr(ele) {
+    if (clicked) {
+
+        // m.style["width"] = "100px";
+        m.style["height"] = "150px";
+        q.style["height"] = "80px";
+        
+        
+        // m.style["width"] = "100px";
+        // m.style["height"] = "100px";
+        // m.style["border"] = "2px solid black";
+        // m.style["border-radius"] = "4px";
+        clicked = false;
+    } else {
+        
+        // m.style["width"] = "10px";
+        m.style["height"] = "0px";
+        q.style["height"] = "0px";
+
+
+        // m.style["width"] = "0px";
+        // m.style["height"] = "0px";
+        // m.style["border"] = "0px";
+        // m.style["border-radius"] = "0px";
+        clicked = true;
+    }
+}
