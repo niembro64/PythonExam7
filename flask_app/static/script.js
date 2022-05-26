58bee6b2bc38fb6f6684120618ffbbc400a10bb6
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-05-26 01:43:04 -0400

    api

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -43,3 +43,50 @@ function growQr(ele) {
         clicked = true;
     }
 }
+
+
+var counterContainer = document.querySelector("#visits");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+
+
+// if(navigator.geolocation){
+//     navigator.geolocation.getCurrentPosition(function(position){
+//         console.log(position.coords.longitude)
+//         console.log(position.coords.latitude)
+
+//         var long = position.coords.longitude;
+//         var lat = position.coords.latitude
+//     })
+// }else{
+//     console.log("Geolocation is not supported by this device/browser")
+// }
+var  ref = document.referrer;
+var  href =  location.href;
+var agent = navigator.userAgent;
+
+fetch("https://ipapi.co/json/")
+.then(response=>response.json())
+.then((responseJson=>{
+
+    // var city = responseJson.city;
+    // var country = responseJson.country; 
+    var location =  responseJson.city + ", " + responseJson.region + " (" + responseJson.country + ")";
+    document.querySelector("#city").innerHTML=location;
+    document.querySelector("#ip").innerHTML=responseJson.ip;
+    // document.querySelector("#region").innerHTML=responseJson.region;
+    // document.querySelector("#country").innerHTML=responseJson.country;
+
+    console.log(responseJson)
+}))
